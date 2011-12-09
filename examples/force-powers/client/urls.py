5377917ce2c450369f2493bb099787db1697740e
exupero <eric.shull@gmail.com>
2011-12-09 16:19:21 -0500

    Cleaned up URLconf.
    Using staticfiles pre-built static file URLs.

--- examples/force-powers/client/urls.py
+++ examples/force-powers/client/urls.py
@@ -1,17 +1,13 @@
-from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
-from django.views.generic.simple import direct_to_template
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = patterns('',
-    (r'^/?$', direct_to_template, {'template': 'index.html'}),
-    url(r'^confidential/', include('confidential.urls')),
-    (r'^static/(?P<path>.*)$',
-     'django.views.static.serve',
-     {'document_root': settings.STATIC_PATH}),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^/?$',
+        'django.views.generic.simple.direct_to_template',
+        {'template': 'index.html'}),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^confidential/', include('confidential.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
